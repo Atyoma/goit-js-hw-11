ac4e4790a14a5539716170ae45acd92e2a8827f9
Artem Kravchenko <atyoma.kravchenko@gmail.com>
2022-01-09 22:39:32 +0200

    add HTTP for API

---
+++ src/fetchCountries.js
@@ -0,0 +1,28 @@
+export default class PhotoApiService {
+  constructor() {
+    this.value = '';
+    this.page = 1;
+  }
+
+  fetchArticles() {
+    console.log(this);
+    const BASE_URL = 'https://pixabay.com/api/';
+    const keyAPI = 'key=25171903-77720667295a00af61497589c';
+
+    return fetch(
+      `${BASE_URL}?${keyAPI}&q=${this.value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`,
+    )
+      .then(response => response.json())
+      .then(data => {
+        this.page += 1;
+      });
+  }
+
+  get query() {
+    return this.value;
+  }
+
+  set query(newValue) {
+    this.value = newValue;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,89 @@
-import './sass/main.scss';
+import './css/styles.css';
+import Notiflix from 'notiflix';
+import PhotoApiService from './fetchCountries';
+
+// const options = {};
+// const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+  searchQuery: document.querySelector('input[name=searchQuery]'),
+  cardContainer: document.querySelector('.gallery'),
+  btnSearch: document.querySelector('.search'),
+  btnLoadMore: document.querySelector('.load-more'),
+};
+console.log(refs.searchForm);
+console.log(refs.searchQuery);
+console.log(refs.cardContainer);
+console.log(refs.btnSearch);
+console.log(refs.btnLoadMore);
+
+const photoApiService = new PhotoApiService();
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.btnLoadMore.addEventListener('click', onLoadMore);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  photoApiService.value = e.currentTarget.elements.searchQuery.value;
+  // const BASE_URL = 'https://pixabay.com/api/';
+  // const keyAPI = 'key=25171903-77720667295a00af61497589c';
+  // return fetch(
+  //   `${BASE_URL}?${keyAPI}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1`,
+  // )
+  //   .then(response => response.json())
+  //   .then(console.log);
+
+  photoApiService.fetchArticles();
+}
+
+function onLoadMore() {
+  photoApiService.fetchArticles();
+}
+
+// let maxCountry = 10;
+// refs.searchBox.addEventListener('input', debounce(onFormInput, DEBOUNCE_DELAY));
+
+// function onFormInput(e) {
+//   console.log(e.target.value);
+//   const stringValue = refs.searchBox.value.trim();
+//   api(stringValue).then(createCountryCard).catch(onFetchErrror);
+// }
+
+// function createCountryCard(countrys) {
+//   console.log(countrys.length);
+//   if (countrys.length > maxCountry) {
+//     Notiflix.Notify.info(`Too many matches found. Please enter a more specific name.`);
+//     return (refs.countryInfo.innerHTML = '');
+//   } else if (countrys.length < 10 && countrys.length !== 1) {
+//     refs.countryInfo.innerHTML = '';
+//     const markup = countrys
+//       .map(
+//         country =>
+//           `<li><div class="country-title"><img src="${country.flags.svg}" alt="${country.name}" width ="40"><h2 class ="title">${country.name.official}</h2></div></li>`,
+//       )
+//       .join('');
+//     refs.countryList.insertAdjacentHTML('beforeend', markup);
+//   } else {
+//     refs.countryInfo.innerHTML = '';
+//     refs.countryList.innerHTML = '';
+//     return countrys
+//       .map(({ name, capital, population, flags, languages }) => {
+//         return (refs.countryInfo.innerHTML = `<div class="country-title"><img src="${
+//           flags.svg
+//         }" alt="${name}" width ="80"><h2 class ="title">${
+//           name.official
+//         }</h2></div><div class="coutry-card"><p class="card-text">Capital: ${capital}</p><p class="card-text">Languages: ${Object.values(
+//           languages,
+//         )}</p><p class="card-text">Population: ${population}</p></div>`);
+//       })
+//       .join('');
+//   }
+// }
+
+// function onFetchErrror(error) {
+//   refs.countryList.innerHTML = '';
+//   refs.countryInfo.innerHTML = '';
+//   Notiflix.Notify.failure(`❌ Oops, there is no country with that name`);
+// }
