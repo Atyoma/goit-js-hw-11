e55d7b1b7168d05d61b6f44439cad25c06bf802b
Artem Kravchenko <atyoma.kravchenko@gmail.com>
2022-01-14 20:25:21 +0200

    finish photo-service

--- src/photo-service.js
+++ src/photo-service.js
@@ -9,23 +9,19 @@ export default class PhotoApiService {
     this.perPage = 40;
   }
 
-  fetchHits() {
+  async fetchHits() {
     axios.defaults.baseURL = 'https://pixabay.com/api/';
     const keyAPI = 'key=25171903-77720667295a00af61497589c';
+    const url = `?${keyAPI}&q=${this.value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${this.perPage}&page=${this.page}`;
+    const response = await axios(url);
+    const data = await response.data;
 
-    return axios(
-      `?${keyAPI}&q=${this.value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${this.perPage}&page=${this.page}`,
-    )
-      .then(response => response.data)
-      .then(data => {
-        this.totalHits = data.totalHits;
-        this.page += 1;
-        if (this.page === 2 && this.totalHits !== 0) {
-          Notiflix.Notify.success(`Hooray! We found ${this.totalHits} images.`);
-        }
-
-        return data.hits;
-      });
+    this.totalHits = data.totalHits;
+    this.page += 1;
+    if (this.page === 2 && this.totalHits !== 0) {
+      Notiflix.Notify.success(`Hooray! We found ${this.totalHits} images.`);
+    }
+    return data.hits;
   }
 
   resetPage() {
